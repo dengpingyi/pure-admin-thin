9c57e6e00e08fc2b84aa0be954057d2dab64388c
dengpingyi <66005355+dengpingyi@users.noreply.github.com>
2022-07-14 10:53:41 +0800

    test: 测试页面
    test: 测试页面

---
+++ src/views/system/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: "systemindex"
+};
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <el-card> 页面A </el-card>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/system/test.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: "systemtest"
+};
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <el-card> 页面B </el-card>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
