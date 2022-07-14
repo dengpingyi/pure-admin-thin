d20545d2d2b93e92a612497ed4a752142f9cff10
dengpingyi <66005355+dengpingyi@users.noreply.github.com>
2022-07-14 10:54:28 +0800

    test: 测试的路由
    test: 测试的路由

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -28,6 +28,53 @@ const remainingRouter = [
         component: () => import("/@/layout/redirect.vue")
       }
     ]
+  },
+  {
+    path: "/system",
+    component: Layout,
+    meta: {
+      icon: "home-filled",
+      title: "设置",
+      rank: 7
+    },
+    children: [
+      {
+        path: "/system/index",
+        name: "systemindex",
+        component: () => import("/@/views/system/index.vue"),
+        meta: {
+          title: "设置a"
+        }
+      },
+      {
+        path: "/system/test",
+        name: "systemtest",
+        component: () => import("/@/views/system/test.vue"),
+        meta: {
+          title: "设置b"
+        }
+      }
+    ]
+  },
+  {
+    path: "/about",
+    component: Layout,
+    meta: {
+      icon: "home-filled",
+      title: "关于",
+      rank: 8
+    },
+    redirect: "/about/index",
+    children: [
+      {
+        path: "/about/index",
+        name: "aboutindex",
+        component: () => import("/@/views/about/index.vue"),
+        meta: {
+          title: "关于"
+        }
+      }
+    ]
   }
 ];
 
