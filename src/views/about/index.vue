24f71d992b689c8543cfb0331e33fd3a8c27b141
dengpingyi <66005355+dengpingyi@users.noreply.github.com>
2022-07-14 10:53:15 +0800

    test: 测试页面
    test: 测试页面

---
+++ src/views/about/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: "aboutindex"
+};
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <el-card> 这是一级菜单的测试 </el-card>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
